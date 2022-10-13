f485d0e6004be8afd507311ff9fe63e45c3f83c6
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-10-13 17:19:19 -0400

    added associations

--- models/stage.js
+++ models/stage.js
@@ -7,8 +7,16 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Event, StageEvent, SetTime }) {
+      Stages.belongsToMany(Event, {
+        foreignKey: "stage_id",
+        as: "events",
+        through: StageEvent,
+      });
+      Stages.hasMany(SetTime, {
+        foreignKey: "stage_id",
+        as: "set_times",
+      });
     }
   }
   Stages.init(
